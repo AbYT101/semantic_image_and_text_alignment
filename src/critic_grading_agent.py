f081402b53325440e465e90b401100673ec6f1da
abyt101 <aberahmyirsaw@gmail.com>
2024-07-12 14:19:04 +0000

    GPT-4o image analysis agnet added

--- src/critic_grading_agent.py
+++ src/critic_grading_agent.py
@@ -4,6 +4,7 @@ import numpy as np
 import os
 from image_analysis import image_agent
 from text_analysis import text_agent
+from openai_image_analysis import openai_image_agent
 
 class CriticGradingAgent(Agent):
     def __init__(self, image_agent, text_agent):
@@ -31,14 +32,14 @@ class CriticGradingAgent(Agent):
         }
 
 # Instantiate the critic/grading agent
-# critic_grading_agent = CriticGradingAgent(image_agent, text_agent)
+critic_grading_agent = CriticGradingAgent(openai_image_agent, text_agent)
 
-# assets_dir = 'data/assets/images'
-# image_files = [os.path.join(assets_dir, f) for f in os.listdir(assets_dir) if f.endswith(('.png', '.jpg', '.jpeg'))]
-# images = [Image.open(file) for file in image_files]
+assets_dir = 'data/assets/images'
+image_files = [os.path.join(assets_dir, f) for f in os.listdir(assets_dir) if f.endswith(('.png', '.jpg', '.jpeg'))]
+images = [Image.open(file) for file in image_files]
 
-# # Example usage
-# image = images[0]
-# text = "Simple advertisement for inspiring travel."
-# analysis = critic_grading_agent.analyze_asset(image, text)
-# print(analysis)
+# Example usage
+image = images[1]
+text = "Simple advertisement for inspiring travel."
+analysis = critic_grading_agent.analyze_asset(image, text)
+print(analysis)
