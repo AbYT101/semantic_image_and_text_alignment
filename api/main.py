069fd5af5276f2d264079c088761a41856808d2e
abyt101 <aberahmyirsaw@gmail.com>
2024-07-15 15:40:07 +0000

    Image composing and api update

--- api/main.py
+++ api/main.py
@@ -1,4 +1,7 @@
 from __init__ import create_app
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.getcwd(), '..')))
 
 app = create_app()
 
--- api/services/evaluate_service.py
+++ api/services/evaluate_service.py
@@ -1,4 +1,24 @@
+import sys
+import os
+from src.text_analysis import text_agent
+from src.image_analysis import image_agent
+from src.critic_grading_agent import CriticGradingAgent
+from PIL import Image
+
+
 def evaluate_image(file, description):
-    # Implement the logic to evaluate the image based on the description
-    evaluation_result = "evaluation_result_placeholder"  # Replace with actual evaluation logic
+    # Load the knowledge base from the file
+    with open('data/design_concepts.txt', 'r') as file:
+        knowledge_base = file.read()
+
+    # Instantiate the critic/grading agent
+    critic_grading_agent = CriticGradingAgent(image_agent, text_agent, knowledge_base)
+
+    # Example usage
+    image = Image.open(file)
+    text =  description
+    # analysis = critic_grading_agent.analyze_asset(image, text)
+    # print(analysis)
+    critic = critic_grading_agent.critic_frame(image, text)
+    evaluation_result = critic
     return evaluation_result
